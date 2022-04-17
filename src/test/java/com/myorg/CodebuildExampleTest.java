3ad136b0069c6399fd5f3fa863e6285698d0dc73
Martin <mart256@gmail.com>
2022-04-17 18:45:03 +0200

    Initial commit

---
+++ src/test/java/com/myorg/CodebuildExampleTest.java
@@ -0,0 +1,26 @@
+// package com.myorg;
+
+// import software.amazon.awscdk.App;
+// import software.amazon.awscdk.assertions.Template;
+// import java.io.IOException;
+
+// import java.util.HashMap;
+
+// import org.junit.jupiter.api.Test;
+
+// example test. To run these tests, uncomment this file, along with the
+// example resource in java/src/main/java/com/myorg/CodebuildExampleStack.java
+// public class CodebuildExampleTest {
+
+//     @Test
+//     public void testStack() throws IOException {
+//         App app = new App();
+//         CodebuildExampleStack stack = new CodebuildExampleStack(app, "test");
+
+//         Template template = Template.fromStack(stack);
+
+//         template.hasResourceProperties("AWS::SQS::Queue", new HashMap<String, Number>() {{
+//           put("VisibilityTimeout", 300);
+//         }});
+//     }
+// }
