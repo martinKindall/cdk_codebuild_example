a4d178bd61e8438bd921ed559fc3ec98b4e6be9a
Martin <mart256@gmail.com>
2022-04-18 17:50:29 +0200

    Changed repository to listen in CodeBuild

--- src/main/java/com/myorg/CodebuildExampleStack.java
+++ src/main/java/com/myorg/CodebuildExampleStack.java
@@ -1,6 +1,9 @@
 package com.myorg;
 
 import software.amazon.awscdk.services.codebuild.*;
+import software.amazon.awscdk.services.iam.AnyPrincipal;
+import software.amazon.awscdk.services.iam.Effect;
+import software.amazon.awscdk.services.iam.PolicyStatement;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
@@ -18,7 +21,7 @@ public class CodebuildExampleStack extends Stack {
 
         var gitHubSource = Source.gitHub(GitHubSourceProps.builder()
                 .owner("martinKindall")
-                .repo("java_codebuild")
+                .repo("aws_spring_cloud_sandbox")
                 .webhook(true)
                 .webhookFilters(List.of(FilterGroup.inEventOf(EventAction.PUSH).andBranchIs("main").andCommitMessageIs(".*version.*")))
                 .build());
@@ -30,5 +33,20 @@ public class CodebuildExampleStack extends Stack {
                         .build())
                 .source(gitHubSource)
                 .build();
+
+        PolicyStatement policyStatement = PolicyStatement.Builder.create()
+                .actions(List.of(
+                        "ecr:BatchCheckLayerAvailability",
+                        "ecr:CompleteLayerUpload",
+                        "ecr:GetAuthorizationToken",
+                        "ecr:InitiateLayerUpload",
+                        "ecr:PutImage",
+                        "ecr:UploadLayerPart"
+                ))
+                .resources(List.of("*"))
+                .effect(Effect.ALLOW)
+                .build();
+
+        build.addToRolePolicy(policyStatement);
     }
 }
